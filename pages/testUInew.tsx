73b45d3204d7480a9e1f67093e7dd00e2008523b
Theo G <gillespietheo@gmail.com>
2024-01-20 10:24:30 -0500

    Updated Test UI

--- pages/testUInew.tsx
+++ pages/testUInew.tsx
@@ -1,14 +1,11 @@
 import Container from "@/components/Container";
 import { useCurrentSession } from "@/lib/client/useCurrentSession";
 
-import { FaBolt, FaMicrophone, FaRobot } from "react-icons/fa";
-import { GiAmplitude, GiRobotGrab } from "react-icons/gi";
-import { BsSpeakerFill } from "react-icons/bs";
-
-import { useState } from "react";
+import { FaArrowRight, FaArrowLeft, FaCube } from "react-icons/fa";
+import { BsCone, BsSpeakerFill } from "react-icons/bs";
+import { MdOutlineCallMissedOutgoing } from "react-icons/md";
+import { GiAmplitude } from "react-icons/gi";
 import { IoPush } from "react-icons/io5";
-
-const AmpDuration = 10*1000;
 
 export default function Homepage() {
 
@@ -16,100 +13,81 @@ export default function Homepage() {
 
     const hide = status === "authenticated";
 
-    const[amped, setAmped] = useState(false);
-    const[ampTime, setAmpTime] = useState(AmpDuration);
-    
-    const ampUp = () => {
-        setAmped(true);
-        setAmpTime(AmpDuration);
-        var start = Date.now();
-        var interval = setInterval(() => {
-         
-           let delta = (Date.now()-start);
-           let val = AmpDuration-delta;
-           if(val <= 0) {
-            clearInterval(interval);
-            setAmped(false);
-            return;
-           }
-           setAmpTime(AmpDuration-delta);
-        }, 10);
-    }
-
-    const [climbedRobots, setClimbedRobots] = useState(0);
-    const [climbed, setClimbed] = useState(false);
-
-    const modifyClimbedRobots = (pos:number) => {
-        if(pos === climbedRobots) {
-            setClimbedRobots(0);
-        } else {
-            setClimbedRobots(pos);
-        } 
-    }
-    
     return <Container requireAuthentication={false} hideMenu={!hide}>
-        <div className="w-full flex flex-col items-center">
+        <div className="w-full flex flex-col items-center space-y-5">
+        
+            <main className="w-full h-full flex-1 flex flex-col items-center">
+                <div className="card h-[650px] w-full bg-base-200 mt-2">
+                    <div className="card-body h-full w-full">
+                        <div className="flex flex-col w-full h-full  items-center justify-center">
+                            <div className="h-1/3 w-full flex flex-row">
+                                <button className="btn btn-outline rounded-none rounded-tl-xl w-1/3 h-full text-lg">
+                                    <GiAmplitude size={60} />
+                                    Scored Amp
+                                </button>
+                                <button className="btn btn-outline rounded-none w-1/3 h-full text-lg">
+                                    <BsSpeakerFill size={60} />
+                                    Scored Speaker
+                                </button>
+                                <button className="btn btn-outline rounded-none rounded-tr-xl w-1/3 h-full text-lg">
+                                    <IoPush size={60} />
+                                    Scored Trap
+                                </button>
+                            </div>
 
-            <div className="card w-full lg:w-1/2 bg-base-300 mt-2 mb-1">
-                <div className="card-body flex flex-col items-center">
-                    
-                    <h1 className="font-bold text-blue-500 text-5xl relative -top-4">#4026</h1>
-
-                    <div role="tablist" className="tabs tabs-boxed w-11/12 flex flex-row justify-between">
-                            <a role="tab" className="tab w-1/3">Auto</a>
-                            <a role="tab" className="tab tab-active w-1/3">Teleop</a>
-                            <a role="tab" className="tab w-1/3">Submit</a>
+                            <div className="h-1/3 w-full flex flex-row">
+                                <button className="btn btn-outline rounded-none rounded-bl-xl w-1/3 h-full text-lg">
+                                    <MdOutlineCallMissedOutgoing size={60} />
+                                    Missed Amp
+                                </button>
+                                <button className="btn btn-outline rounded-none w-1/3 h-full text-lg">
+                                <MdOutlineCallMissedOutgoing size={60} />
+                                    Missed Speaker
+                                </button>
+                                <button className="btn btn-outline rounded-none rounded-br-xl w-1/3 h-full text-lg">
+                                    <MdOutlineCallMissedOutgoing size={60} />
+                                    Missed Trap
+                                </button>
+                            </div>
                         </div>
 
-                    <div className="card-actions justify-between w-full">
-                        
+                        <div className="w-full h-1/3 text-center">
+                            <h1 className="text-xl font-bold mb-2">Defense</h1>
+                            <input type="range" min={0} max={2} value={1} className="range range-primary" step="1" />
+                            <div className="w-full flex justify-between text-lg px-2">
+                                <span>None</span>
+                                <span>Partial</span>
+                                <span>Full</span>
+                                
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-            </div>
+            </main>
+            <footer className="w-full">
+                <div className="card w-full bg-base-200">
+                    <div className="card-body flex flex-col items-center">
+                        <h2 className=" text-blue-500 font-bold text-5xl">#4026</h2>
 
+                        <div className="card-actions justify-between w-full">
+                            <button className="btn btn-primary">
+                                <FaArrowLeft />
+                                Auto
+                            </button>
 
-            {amped ? <div className="w-full lg:w-1/2 rounded-full h-2.5 dark:bg-gray-700">
-                        <div className="bg-primary h-2.5 rounded-full drop-shadow-glowStrong" style={{width: `${(ampTime/AmpDuration)*100}%`}}></div>
-                    </div>: <progress className="progress w-full"></progress>
-            }
+                            <h2 className="font-bold text-3xl">Teleop</h2>
 
-            <div className="card w-full lg:w-1/2 bg-base-300 shadow-xl mt-1 pb-2">
-                <div className="card-body flex flex-col items-center">
-
-                    <h1 className="text-3xl font-bold">Teleop</h1>
-
-                    <button className={`btn ${amped ? "btn-disabled" : "btn-active"} btn-primary btn-wide drop-shadow-glowWeak text-xl mt-4`} onClick={ampUp}><FaBolt /> {amped ? "Amped": "Amp Up"}</button>
-                    <button className={`btn btn-active btn-primary btn-wide  text-xl mt-2`} onClick={()=>{}}><GiAmplitude className="text-3xl" /> Scored Amp</button>
-                    <button className={`btn btn-active btn-primary btn-wide ${amped ? "drop-shadow-glowStrong" : ""} text-xl mt-2`}><BsSpeakerFill /> Scored Speaker </button>
-
-                    <div className="divider"></div>
-
-                    <h1 className="text-3xl font-bold">Endgame</h1>
-                    
-                    <div className="w-3/4 flex flex-row justify-between items-center mt-2">
-                        
-                        <h1 className="text-2xl font-bold"> <GiRobotGrab className="inline relative -top-1" /> Climbed</h1>
-                        <input type="checkbox" className="toggle toggle-primary" checked={climbed} onChange={()=>{setClimbed((prev)=>!prev)}}/>
+                            <button className="btn btn-primary">
+                                Endgame
+                                <FaArrowRight />
+                            </button>
+                        </div>
                     </div>
-
-                    <div className="w-3/4 flex flex-row justify-between items-center">
-                        
-                        <h1 className="text-2xl font-bold"> <FaMicrophone className="inline relative -top-1" /> Microphone</h1>
-                        <input type="checkbox" className="toggle toggle-primary" checked={climbed} onChange={()=>{setClimbed((prev)=>!prev)}}/>
-                    </div>
-
-                    <button className={`btn btn-active btn-primary btn-wide  text-xl mt-2`} onClick={()=>{}}><IoPush className="text-3xl" /> Scored Trap</button>
-
-                    <h1 className="text-xl font-bold mt-3">Robots on Stage</h1>
-                    <div className="rating gap-1">
-                        <button className={`btn btn-ghost ${climbedRobots >= 1 ? "" : "opacity-20"} h-fit`} onClick={()=>{modifyClimbedRobots(1)}}><FaRobot size={60}></FaRobot></button>
-                        <button className={`btn btn-ghost ${climbedRobots >= 2 ? "" : "opacity-20"} h-fit`} onClick={()=>{modifyClimbedRobots(2)}}><FaRobot size={60}></FaRobot></button>
-                        <button className={`btn btn-ghost ${climbedRobots == 3 ? "" : "opacity-20"} h-fit`} onClick={()=>{modifyClimbedRobots(3)}}><FaRobot size={60}></FaRobot></button>
-                    </div>
-
                 </div>
-            </div>
-        </div>
+            </footer>
+    </div>
+
            
     </Container>
 
