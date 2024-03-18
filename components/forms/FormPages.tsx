6a5ba3ab45bc8f6ff1c223b00b9e59c44aefccac
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-18 17:27:49 -0400

    Used string interpolation

--- components/forms/FormPages.tsx
+++ components/forms/FormPages.tsx
@@ -25,7 +25,7 @@ export default function FormPage(props: {
 }) {
   return (
     <main className="w-full h-full flex-1">
-      <div className={"card w-full h-" + (props.height ?? "[650px]") + " bg-base-200 mt-2" }>
+      <div className={`card w-full h-${(props.height ?? "[650px]")} bg-base-200 mt-2`}>
         <div className="card-body h-full w-full flex flex-col items-center">
           <h1 className="text-5xl font-bold">{props.title}</h1>
           <hr className="w-2/3 border-slate-700 border-2"></hr>
