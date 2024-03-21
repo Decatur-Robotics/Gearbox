cbce9e8847d2fb17c45b1939747c9494e86b430a
Theo G <gillespietheo@gmail.com>
2024-03-21 00:18:55 -0400

    bad solution

--- components/forms/ImageUpload.tsx
+++ components/forms/ImageUpload.tsx
@@ -1,67 +1,90 @@
-import { Pitreport } from '@/lib/Types';
-import imageCompression from 'browser-image-compression';
-import React, { useState } from 'react';
-import { FaFileUpload, FaImage } from 'react-icons/fa';
+import { Pitreport } from "@/lib/Types";
+import imageCompression from "browser-image-compression";
+import React, { useState } from "react";
+import { FaFileUpload, FaImage } from "react-icons/fa";
 
 const CompressionOptions = {
-    maxSizeMB: 0.5,
-    maxWidthOrHeight: 1920,
-    useWebWorker: true,
-}
+  maxSizeMB: 0.5,
+  maxWidthOrHeight: 1920,
+  useWebWorker: true,
+};
 
-export default function ImageUpload(props: {data: Pitreport, callback: (key: string, value: string | number | boolean) => void}) {
+export default function ImageUpload(props: {
+  data: Pitreport;
+  callback: (key: string, value: string | number | boolean) => void;
+}) {
+  const [imageUrl, setImageUrl] = useState(
+    props.data.image !== "/robot.jpg" ? props.data.image : ""
+  );
+  const [uploadProgress, setUploadProgress] = useState(-1);
 
-  const[imageUrl, setImageUrl] = useState(props.data.image !== "/robot.jpg"?props.data.image: "");
-  const[uploadProgress, setUploadProgress] = useState(-1);
-
-  const onUpload = async(event: any) => {
+  const onUpload = async (event: any) => {
     setUploadProgress(50);
     const data = event.target.files[0];
     console.log(`originalFile size ${data.size / 1024 / 1024} MB`);
     const compressedFile = await imageCompression(data, CompressionOptions);
     console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`);
 
-
-    
     const formData = new FormData();
-    formData.append('files', compressedFile);
+    formData.append("files", compressedFile);
 
-    const res = await fetch('/api/img/upload', {
-        method: 'POST',
-        headers: {
-            "gearbox-auth": "gearboxiscool",
-        },
-        body: formData,
+    const res = await fetch("/api/img/upload", {
+      method: "POST",
+      headers: {
+        "gearbox-auth": "gearboxiscool",
+      },
+      body: formData,
     });
 
-    if(res.status === 200) {
-        const data = await res.json();
-        const url = `/api/img/${data.filename}`;
+    if (res.status === 200) {
+      const data = await res.json();
+      const url = `/api/img/${data.filename}`;
+
+      setTimeout(() => {
         setImageUrl(url);
-        props.callback("image", url);
-        setUploadProgress(100);
+      }, 1000);
+
+      props.callback("image", url);
+      setUploadProgress(100);
     } else {
-        setUploadProgress(-1)
+      setUploadProgress(-1);
     }
-
-
-
   };
 
   return (
-    <div className='w-full flex flex-col items-center'>
-        {!imageUrl ? <div className='flex flex-col justify-center items-center w-64 h-64 animate-pulse bg-slate-600 opacity-25 rounded-lg'>
-            <FaImage size={60}></FaImage>
-            Upload an Image first
-        </div>: <img src={imageUrl} className='w-64 h-64 rounded-lg'></img>}
-        {uploadProgress > 0 ? <progress className="progress w-64 my-2" value={uploadProgress} max="100"></progress>: <></>}
-        <label className="form-control w-full max-w-xs">
-            <div className="label">
-                <span className="label-text inline"><FaFileUpload className='inline mr-2'></FaFileUpload>Pick a Image</span>
-                <span className="label-text-alt">Only .png, .jpg, jpeg accepted</span>
-            </div>
-            <input type="file" className="file-input file-input-bordered w-full max-w-xs" accept=".png,.jpg,.jpeg" multiple={false} onChange={onUpload} />
-        </label>
+    <div className="w-full flex flex-col items-center">
+      {!imageUrl ? (
+        <div className="flex flex-col justify-center items-center w-64 h-64 animate-pulse bg-slate-600 opacity-25 rounded-lg">
+          <FaImage size={60}></FaImage>
+          Upload an Image first
+        </div>
+      ) : (
+        <img src={imageUrl} className="w-64 h-64 rounded-lg"></img>
+      )}
+      {uploadProgress > 0 ? (
+        <progress
+          className="progress w-64 my-2"
+          value={uploadProgress}
+          max="100"
+        ></progress>
+      ) : (
+        <></>
+      )}
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text inline">
+            <FaFileUpload className="inline mr-2"></FaFileUpload>Pick a Image
+          </span>
+          <span className="label-text-alt">Only .png, .jpg, jpeg accepted</span>
+        </div>
+        <input
+          type="file"
+          className="file-input file-input-bordered w-full max-w-xs"
+          accept=".png,.jpg,.jpeg"
+          multiple={false}
+          onChange={onUpload}
+        />
+      </label>
     </div>
   );
 }
