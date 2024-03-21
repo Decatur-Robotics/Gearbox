abfab5b3a2743032803e7480044acebe8834ec7f
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-21 16:18:40 -0400

    Removed ellipsis from image route

--- components/forms/ImageUpload.tsx
+++ components/forms/ImageUpload.tsx
@@ -9,8 +9,6 @@ const DefaultCompressionOptions = {
   useWebWorker: true,
 };
 
-export const dynamic = "force-dynamic";
-
 export default function ImageUpload(props: {
   data: Pitreport;
   callback: (key: string, value: string | number | boolean) => void;
