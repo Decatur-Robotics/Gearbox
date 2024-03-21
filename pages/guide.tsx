f749d912ed7878db5c5fb2436adea208ad6f5809
Theo G <gillespietheo@gmail.com>
2024-03-21 17:15:44 -0400

    Davis Guide

--- pages/guide.tsx
+++ pages/guide.tsx
@@ -10,10 +10,7 @@ export default function Guide() {
       <div className="w-full h-full flex justify-center">
         <div className="w-full md:w-1/2 min-h-screen flex flex-col items-center mt-4 indent-8">
           <h1 className="text-4xl font-bold">Gearbox Team Guide</h1>
-          <p>
-            This quick guide will help you setup Gearbox for your team{"'"}s
-            needs
-          </p>
+          <p>This quick guide will help you get up and running with Gearbox.</p>
           <div className="divider"></div>
           <p>
             Gearbox is a tool designed to make your scouting workforce as
@@ -44,31 +41,31 @@ export default function Guide() {
             and clicking the link to create your team.{" "}
             <span className="font-semibold">
               <br></br>
-              This may require you to sign in.
+              This requires you to create an account/sign in.
             </span>{" "}
           </p>
           <br></br>
           <p>
-            Scrolling down, you can then search your teams number, click the
-            result, and Gearbox will generate a new team that is prefilled with
+            After reaching your profile you can then click the create a team
+            button, then search for your teams number, click the your teams
+            name, and Gearbox will generate a new team that is prefilled with
             relevant properties.
           </p>
           <h1 className="text-xl font-semibold mt-2">Joining a Team</h1>
           <p>
-            Alternatively if your team already has been added to gearbox you can
-            request to join it via the join team search tool. This allows you to
-            send a request to a join a team and then your teams management can
-            accept you.
+            If your team has already joined gearbox you can request to be added
+            to it via the join team searchbox. Type your teams number into the
+            searchbox then click the team name to send a join request.
           </p>
 
           <div className="divider"></div>
           <h1 className="text-3xl font-semibold mb-2">Managing a Team</h1>
           <img src="/roster.png" className="w-1/2"></img>
           <p>
-            Managers of a team have the special ability to appoint other
-            managers which can toggle who is included in the scouting schedule
-            and can also manage requests to the team. Additionally a team
-            manager can change the teams settings and create
+            Team managers have the ability to appoint other managers, this means
+            you can appoint other trusted members of your team to help manage
+            your scouting. Managers can toggle who is assigned to scout, manage
+            requests to join the team, change the teams settings, and create
             seasons/competitions.
           </p>
 
@@ -77,44 +74,47 @@ export default function Guide() {
           <img src="/season.png" className="w-1/2"></img>
           <p>
             Gearbox divides your competitions into various seasons in order to
-            make everything scalable and organized. For right now, there are
-            only two options, the{" "}
+            make everything scalable and organized. Currently there are only two
+            options, the{" "}
             <span className="font-semibold">
-              off-season and the current Crescendo season{" "}
+              off-season and the current season, Crescendo for us.{" "}
             </span>
-            . The off-season option is great for testing and off-season
-            competitions and but Crescendo can be the home for your official
-            competitions.
+            The off-season option is great for getting used to gearbox and
+            scouting off-season competitions, but you should use the current
+            season, crescendo, for official competitions.
           </p>
 
           <div className="divider"></div>
           <h1 className="text-3xl font-semibold mb-2">Competitions</h1>
           <img src="/createComp.png" className="w-1/2"></img>
           <p>
-            Competitions hold all of your relavant data concerning the your
-            current matches and particpating teams. Statistics on your scouters
-            performances will be displayed at the start of page along with
-            buttons to view stats, pit-stats (our deployable slideshow for pit
-            deployment) and relevant rankings. When match data is avaiable you
-            can optionally click the refresh button which will pull the latest
-            data from FRC servers. Once data is available, you can then assign
-            scouters which will ensure that every selected scouter will have a
-            set of matches.
+            Competitions hold all of the relavant data concerning your current
+            matches and particpating teams. Statistics on your scouting
+            performance will be displayed at the top of the page along with
+            buttons to view your competition stats, pit-stats our slideshow for
+            pit display and relevant rankings. When match data is available you
+            can click the refresh button which will attempt to pull the latest
+            match data from FRC servers and automatically generate matches. Once
+            data is available, you can then assign scouters which, will generate
+            a set of matches for every selected scouter to cover.
           </p>
           <br></br>
           <p>
-            Pitscouting forms are available to any member of the team and once
-            clicked, will provide a seperate form where the user can upload an
-            image of the robot plus its capabilities.
+            Pitscouting forms are available, which any member of the team can
+            access. Pitscouting provides a seperate form you can upload an image
+            of the robot and record its capabilities such as drivetrain, climber
+            status, and ability to fit under the stage.
           </p>
 
           <div className="divider"></div>
           <h1 className="text-3xl font-semibold mb-2">Manual Match Creation</h1>
           <img src="/createComp.png" className="w-1/2"></img>
           <p>
-            In the abscence of automatically provided data, a manager can
-            manually create matches by entering the various teams and the match
-            number. Afterwards you may need to re-assign scouters.
+            In the event that gearbox does not have access to match data
+            provided by FIRST, a team manager can manually create matches by
+            entering the teams playing in the match and the match number. You
+            will need to re-assign scouters after manually creating a match, as
+            the match wont have any scouters assigned by default.
           </p>
         </div>
       </div>
