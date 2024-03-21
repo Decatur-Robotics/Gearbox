64f2653f5e90a8291a972382b6f5703339cba18f
Theo G <gillespietheo@gmail.com>
2024-03-21 01:01:53 -0400

    debug

--- pages/api/img/[...image].ts
+++ pages/api/img/[...image].ts
@@ -13,15 +13,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.warn("invocked");
   if (req.method === "GET") {
+    console.warn("GET");
     var filename = req.url?.split("/api/img/")[1];
     if (!filename) return res.send({ status: 400, message: "Invalid Request" });
     const db = await GetDatabase();
     //@ts-ignore
     const bucket = new GridFSBucket(db.db, { bucketName: "bucket" });
+    console.log(filename);
     try {
+      console.log("filing");
       bucket.openDownloadStreamByName(filename).pipe(res);
     } catch (e) {
+      console.log(":error");
       return res.send({ status: 400, message: e });
     }
   } else {
