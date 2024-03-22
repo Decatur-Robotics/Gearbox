ba498c8da3d899f184ccadda9ff3c91dbe8f285e
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-22 19:03:13 -0400

    Updated alert

--- components/Container.tsx
+++ components/Container.tsx
@@ -9,6 +9,7 @@ import ClientAPI from "@/lib/client/ClientAPI";
 import Footer from "./Footer";
 import { FaDiscord, FaSearch } from "react-icons/fa";
 import useCheckMobile from "@/lib/client/useCheckMobile";
+import { MdWarning } from "react-icons/md";
 
 const api = new ClientAPI("gearboxiscool");
 
@@ -114,19 +115,13 @@ export default function Container(props: ContainerProps) {
     >
       <div
         role="alert"
-        className="alert rounded-none py-1 sm:py-2 font-semibold alert-info"
+        className="alert rounded-none py-1 sm:py-2 font-semibold alert-warning"
       >
-        <FaDiscord className="max-sm:hidden" size={32} />
+        <MdWarning className="max-sm:hidden" size={32} />
       
         <span className="max-sm:text-sm">
-          Alert: Join our Discord server to receive important announcements and receive support!
+          Alert: Blue Alliance data may be out of date.
         </span>
-          <a
-            href="https://discord.gg/ha7AnqxFDD"
-            className="btn btn-primary ml-4 max-sm:mt-1"
-          >
-            <FaDiscord size={24} /> Join today
-          </a>
       </div>
 
       <div className="drawer">
