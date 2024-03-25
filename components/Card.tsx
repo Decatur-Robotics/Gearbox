0600f452a3ac3e5039e00b2d8abb0a4e8c6acfd9
Theo G <gillespietheo@gmail.com>
2024-03-25 18:30:34 -0400

    Better Profile Page

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,36 @@
 import { ReactNode } from "react";
 
-export type CardProps = {title: string | undefined, className: string | undefined, children: ReactNode};
+export type CardProps = {
+  title?: string | undefined;
+  className?: string | undefined;
+  children: ReactNode;
+  coloredTop?: "primary" | "secondary" | "accent" | undefined;
+};
 export default function Card(props: CardProps) {
   const title = props.title;
   const className = props.className;
-  const children = props.children
+  const children = props.children;
+  const color = props.coloredTop;
 
-  return <div className={`card bg-base-200 shadow-xl w-1/2 max-sm:w-5/6 ${className}`}>
-    <div className="card-body">
-      {title ? <h1 className="card-title text-3xl font-semibold">{title}</h1> : <></>}
-      {children}
+  return (
+    <div
+      className={`card bg-base-200 shadow-xl w-2/3 max-sm:w-5/6 ${className}`}
+    >
+      {color ? (
+        <div
+          className={`card-body min-h-1/2 w-full bg-${color} rounded-t-lg`}
+        ></div>
+      ) : (
+        <></>
+      )}
+      <div className="card-body">
+        {title ? (
+          <h1 className="card-title text-3xl font-semibold">{title}</h1>
+        ) : (
+          <></>
+        )}
+        {children}
+      </div>
     </div>
-  </div>
+  );
 }
