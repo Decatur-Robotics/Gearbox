583fb41d4fae9805547e5a6f4a2e6a0b134eedfb
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-30 13:35:04 -0400

    Added build_on_change

---
+++ build_on_change.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+REMOTE=origin
+BRANCH=master
+git fetch
+if [[ "$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]]; then
+  echo "Changes detected"
+  git pull
+  npm ci
+  npm run build
+  kill $(lsof -t -i:443)
+  npm run start
+fi
--- update.sh
+++
@@ -1,4 +0,0 @@
-git pull
-npm ci
-npm run build
-npm run start
