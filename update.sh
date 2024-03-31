5151ed8c7548816b0914395ef61067ca7da283bf
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-31 16:02:59 -0400

    Added NODE var to update

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 git stash
 git pull
 git stash apply
-/home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm ci
-/home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm run build
+NODE=/home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm
+$NODE ci
+$NODE run build
 pid=$(sudo lsof -t -i:443)
 echo "Killing process $pid..."
 sudo kill $pid
 TIMEOUT_SECS=10
 timeout $TIMEOUT_SECS tail --pid=$pid -f /dev/null
-/home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm run start
+sudo setcap 'cap_net_bind_service=+ep' $(readlink -f $NODE)
+$NODE run start
