70d800223073bfaffd52397c0c3947f90258cea4
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-31 15:09:06 -0400

    More logs

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@ git pull
 /home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm ci
 /home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm run build
 pid=$(sudo lsof -t -i:443)
+echo "Killing process $pid..."
 sudo kill $PID
 TIMEOUT_SECS=10
 timeout $TIMEOUT_SECS tail --pid=$pid -f /dev/null
