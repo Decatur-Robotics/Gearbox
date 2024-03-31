3ee32104b570663aeec4457c208a28b4d943d936
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-31 15:06:11 -0400

    Now waits for process to die

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 git pull
 /home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm ci
 /home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm run build
-PID=$(sudo lsof -t -i:443)
+pid=$(sudo lsof -t -i:443)
 sudo kill $PID
+TIMEOUT_SECS=10
+timeout $TIMEOUT_SECS tail --pid=$pid -f /dev/null
 /home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm run start
