cb90838b2c0e2205d84726bb7306f9b1ecc804d2
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-31 15:24:55 -0400

    Fixed capitalization

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ git pull
 /home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm run build
 pid=$(sudo lsof -t -i:443)
 echo "Killing process $pid..."
-sudo kill $PID
+sudo kill $pid
 TIMEOUT_SECS=10
 timeout $TIMEOUT_SECS tail --pid=$pid -f /dev/null
 /home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm run start
