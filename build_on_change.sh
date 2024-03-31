3ead3c929045ccccfa11635158e7f6e5fa494418
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-31 15:38:02 -0400

    Update happens in background now

--- build_on_change.sh
+++ build_on_change.sh
@@ -6,7 +6,7 @@ cd ./Gearbox
 git fetch
 if [[ "$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]]; then
   echo "Changes detected"
-  bash update.sh
+  bash update.sh &
 else
   echo "No changes detected"
 fi
