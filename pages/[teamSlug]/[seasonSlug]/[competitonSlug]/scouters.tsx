ac35a859c34307bdfcd89479d5d244fee605d5d6
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-04-05 22:38:47 -0400

    Added comments to scouter management

--- pages/[teamSlug]/[seasonSlug]/[competitonSlug]/scouters.tsx
+++ pages/[teamSlug]/[seasonSlug]/[competitonSlug]/scouters.tsx
@@ -19,24 +19,35 @@ export default function Scouters(props: { team: Team | null, competition: Compet
     : false;
 
   type Scouter = User & { missedReports: string[], reports: string[] };
+  type Comment = {
+    text: string,
+    user: string | undefined,
+    robot: number,
+    match: string,
+    report: string,
+    flag: "None" | "Minor" | "Major"
+  }
 
-  const [scouters, setScouters] = useState<Scouter[] | undefined>();
+  const [scouters, setScouters] = useState<{ [id: string]: Scouter } | undefined>();
+  const [shouldRegenerateScouterData, setShouldRegenerateScouterData] = useState<boolean>(false);
+
   const [matches, setMatches] = useState<{ [id: string]: Match } | undefined>();
   const [reports, setReports] = useState<{ [id: string]: Report } | undefined>();
   const [lastCountedMatch, setLastCountedMatch] = useState<number>(comp?.matches.length || 0);
-  const [shouldRegenerateScouterData, setShouldRegenerateScouterData] = useState<boolean>(false);
+
+  const [comments, setComments] = useState<Comment[] | undefined>();
 
   useEffect(() => {
     if (!scouters) {
       team?.scouters.forEach(async (scouter) => {
         const user = await api.findUserById(scouter);
-        setScouters((prev) => [...(prev || []), {
+        setScouters((prev) => ({...(prev || {}), [scouter]: {
           ...user,
           missedReports: [],
           reports: []
-        }]);
+        }}));
       });
-      setScouters([]);
+      setScouters({});
     }
 
     if (!matches) {
@@ -55,6 +66,27 @@ export default function Scouters(props: { team: Team | null, competition: Compet
           setReports((prev) => {
             if (Object.keys(prev || {}).length + 6 >= lastCountedMatch * 6)
               setShouldRegenerateScouterData(true);
+
+            if (r.submitted) {
+              const text = r.data.comments;
+
+              let flag: "None" | "Minor" | "Major" = "None";
+              if (text.length === 0) flag = "Minor";
+
+              // const regex = /^[~`!@#$%^&*()_+=[\]\\{}|;':",.\/<>?a-zA-Z0-9-]+$/;
+              // if ("aこんにちは".match(regex) === null) flag = "Minor";
+
+              const comment: Comment = {
+                text: text,
+                user: r.user,
+                robot: r.robotNumber,
+                match: r.match,
+                report: report,
+                flag: flag
+              }
+              setComments((prev) => [...(prev || []), comment]);
+            }
+
             return { ...(prev || {}), [report]: r }
           });
         });
@@ -67,7 +99,7 @@ export default function Scouters(props: { team: Team | null, competition: Compet
     if (shouldRegenerateScouterData && scouters && matches && reports) {
       console.log("Regenerating scouter data...");
 
-      const newScouters = scouters.map((scouter) => {
+      const newScouters = Object.values(scouters).map((scouter) => {
         const scouterReports = Object.values(reports).filter((report) => {
           return matches[report.match].number <= lastCountedMatch && report.user === scouter._id;
         });
@@ -83,7 +115,12 @@ export default function Scouters(props: { team: Team | null, competition: Compet
         };
       });
 
-      setScouters(newScouters);
+      const scouterDict: { [id: string]: Scouter } = {};
+      for (const scouter of newScouters) {
+        scouterDict[scouter._id ?? ""] = scouter;
+      }
+
+      setScouters(scouterDict);
       setShouldRegenerateScouterData(false);
     }
   }, [shouldRegenerateScouterData, lastCountedMatch]);
@@ -98,11 +135,11 @@ export default function Scouters(props: { team: Team | null, competition: Compet
           </div>
         </div>
          : <div className="flex flex-row max-sm:flex-col">
-            <div className="card w-1/3 max-sm:w-2/3 bg-base-200 m-12">
+            <div className="card w-1/3 max-sm:w-3/4 bg-base-200 m-12">
               <div className="card-body">
                 <h1 className="card-title">Scouters</h1>
                 <p>
-                  Scouters: {scouters?.length}<br />
+                  Scouters: {scouters && Object.keys(scouters)?.length}<br />
                   Matches: {matches && Object.keys(matches)?.length}/{comp && comp?.matches.length} (loaded/total)<br />
                   Reports:{" "}
                     {reports && Object.values(reports).filter(r => r.submitted).length}
@@ -112,7 +149,7 @@ export default function Scouters(props: { team: Team | null, competition: Compet
                   <span className="tooltip" data-tip="Reports for this match and any before it that are not submitted will be considered missing.">
                     Last Counted Match:
                   </span>
-                  <input type="number" value={lastCountedMatch} className="input input-bordered input-sm ml-2 w-1/6"
+                  <input type="number" value={lastCountedMatch} className="input input-bordered input-sm ml-2 w-1/5 sm:w-1/6"
                     onChange={(e) => {
                       setShouldRegenerateScouterData(true);
                       setLastCountedMatch(Math.max(Math.min(parseInt(e.target.value), comp?.matches.length ?? 0), 0));
@@ -120,7 +157,7 @@ export default function Scouters(props: { team: Team | null, competition: Compet
                 </p>
                 <ul>
                 {
-                  scouters?.filter((scouter) => scouter.reports.length > 0).map((scouter) => <li key={scouter._id}>
+                  scouters && Object.values(scouters)?.filter((scouter) => scouter.reports.length > 0).map((scouter) => <li key={scouter._id}>
                     <span className={scouter.missedReports.length > 0 ? "text-warning" : ""}>{scouter.name}</span>
                     <ul className="text-sm ml-2 mb-1">
                       <li>Missed Reports: {scouter.missedReports.length}
@@ -143,7 +180,27 @@ export default function Scouters(props: { team: Team | null, competition: Compet
                 </ul>
               </div>
             </div>
-         </div>
+            <div className="card w-1/3 max-sm:w-3/4 bg-base-200 m-12">
+              <div className="card-body">
+                <h1 className="card-title">Comments</h1>
+                <ul>
+                  {
+                    scouters && matches && comments && comments.sort((a, b) => matches[a.match].number - matches[b.match].number)
+                      .map((comment) => <li className="mb-1" key={comment.report}>
+                      <span className={comment.flag === "Major" ? "text-error" : comment.flag === "Minor" ? "text-warning" : ""}>
+                        {comment.text !== "" ? comment.text : "[Empty Comment]"}
+                      </span>
+                      <ul className="text-sm ml-2">
+                        <li>Match: {matches[comment.match].number}</li>
+                        <li>Robot: {comment.robot}</li>
+                        <li>Scouter: {comment.user ? scouters[comment.user].name : "Unknown"}</li>
+                      </ul>
+                    </li>)
+                  }
+                </ul>
+              </div>
+            </div>
+        </div>
     }
   </Container>);
 }
