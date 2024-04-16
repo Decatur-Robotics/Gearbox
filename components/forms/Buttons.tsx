27b6d507ae4145d72678c9c3bfba6f071afad11e
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-04-16 17:43:12 -0400

    Undo buttons and built-in labels

--- components/forms/Buttons.tsx
+++ components/forms/Buttons.tsx
@@ -4,68 +4,42 @@ export type ButtonProps = {
   callback: (key: string, value: string | number | boolean) => void;
 };
 
+// Can't use key as a prop
+function IncrementButton(
+  props: { dataKey: string, data: FormData, 
+    callback: (key: string, value: string | number | boolean) => void, text: string, rounded?: string }) {
+  
+  const roundTop = props.rounded?.includes("t");
+  const roundBottom = props.rounded?.includes("b");
+
+  return (
+    <div className="flex flex-col h-full pb-6">
+      <button
+        onClick={() => {
+          props.callback(props.dataKey, props.data[props.dataKey] + 1);
+        }}
+        className={`btn btn-outline active:bg-blue-300 rounded-none w-full h-[80%] text-lg ${roundTop && props.rounded && `rounded-${props.rounded}-xl`}`}
+      >
+        {props.text}: {props.data[props.dataKey]}
+      </button>
+      <button onClick={() => {
+          props.callback(props.dataKey, Math.max(props.data[props.dataKey] - 1, 0));
+        }}
+        className={`btn btn-outline active:bg-red-300 rounded-none h-[20%] ${roundBottom && props.rounded && `rounded-${props.rounded}-xl`}`}>
+        Undo
+      </button>
+    </div>
+  );
+}
+
 export function AutoButtons(props: ButtonProps) {
   return (
     <div className="w-full h-full flex flex-col items-center">
-      <div className="w-full h-16 bg-base-300 rounded-t-xl flex flex-row font-mono border-4 border-neutral">
-        <div className="w-1/2 text-md flex flex-row items-center justify-center text-center">
-          <p className="">
-            Scored: <span className="block">{props.data.AutoScoredAmp}</span>
-          </p>
-          <p>
-            Missed: <span className="block">{props.data.AutoMissedAmp}</span>
-          </p>
-        </div>
-        <div className="w-1/2 text-md flex flex-row items-center justify-center text-center">
-          <p>
-            Scored:{" "}
-            <span className="block">{props.data.AutoScoredSpeaker}</span>
-          </p>
-          <p>
-            Missed:{" "}
-            <span className="block">{props.data.AutoMissedSpeaker}</span>
-          </p>
-        </div>
-      </div>
       <div className="w-full h-2/3 grid grid-cols-2 grid-rows-2">
-        <button
-          onClick={() => {
-            props.callback("AutoScoredAmp", props.data.AutoScoredAmp + 1);
-          }}
-          className="btn btn-outline rounded-none w-full h-full text-lg"
-        >
-          Scored Amp
-        </button>
-        <button
-          onClick={() => {
-            props.callback(
-              "AutoScoredSpeaker",
-              props.data.AutoScoredSpeaker + 1
-            );
-          }}
-          className="btn btn-outline rounded-none w-full h-full text-lg"
-        >
-          Scored Speaker
-        </button>
-        <button
-          onClick={() => {
-            props.callback("AutoMissedAmp", props.data.AutoMissedAmp + 1);
-          }}
-          className="btn btn-outline rounded-none w-full h-full text-lg rounded-bl-xl"
-        >
-          Missed Amp
-        </button>
-        <button
-          onClick={() => {
-            props.callback(
-              "AutoMissedSpeaker",
-              props.data.AutoMissedSpeaker + 1
-            );
-          }}
-          className="btn btn-outline rounded-none w-full h-full text-lg rounded-br-xl"
-        >
-          Missed Speaker
-        </button>
+        <IncrementButton dataKey="AutoScoredAmp" data={props.data} callback={props.callback} text="Scored Amp" rounded="tl" />
+        <IncrementButton dataKey="AutoScoredSpeaker" data={props.data} callback={props.callback} text="Scored Speaker" rounded="tr" />
+        <IncrementButton dataKey="AutoMissedAmp" data={props.data} callback={props.callback} text="Missed Amp" rounded="bl" />
+        <IncrementButton dataKey="AutoMissedSpeaker" data={props.data} callback={props.callback} text="Missed Speaker" rounded="br" />
       </div>
     </div>
   );
@@ -74,90 +48,13 @@ export function AutoButtons(props: ButtonProps) {
 export function TeleopButtons(props: ButtonProps) {
   return (
     <div className="w-full h-full flex flex-col items-center">
-      <div className="w-full h-16 bg-base-300 rounded-t-xl flex flex-row font-mono border-4 border-neutral">
-        <div className="w-1/2 text-xs flex flex-row items-center justify-center text-center">
-          <p className="">
-            Scored: <span className="block">{props.data.TeleopScoredAmp}</span>
-          </p>
-          <p>
-            Missed: <span className="block">{props.data.TeleopMissedAmp}</span>
-          </p>
-        </div>
-        <div className="w-1/2 text-xs flex flex-row items-center justify-center text-center">
-          <p className="">
-            Scored:{" "}
-            <span className="block">{props.data.TeleopScoredSpeaker}</span>
-          </p>
-          <p>
-            Missed:{" "}
-            <span className="block">{props.data.TeleopMissedSpeaker}</span>
-          </p>
-        </div>
-        <div className="w-1/2 text-xs flex flex-row items-center justify-center text-center">
-          <p>
-            Scored: <span className="block">{props.data.TeleopScoredTrap}</span>
-          </p>
-          <p>
-            Missed: <span className="block">{props.data.TeleopMissedTrap}</span>
-          </p>
-        </div>
-      </div>
-      <div className="w-full h-2/3 grid grid-cols-3 grid-rows-2">
-        <button
-          onClick={() => {
-            props.callback("TeleopScoredAmp", props.data.TeleopScoredAmp + 1);
-          }}
-          className="btn btn-outline rounded-none w-full h-full text-lg"
-        >
-          Scored Amp
-        </button>
-        <button
-          onClick={() => {
-            props.callback(
-              "TeleopScoredSpeaker",
-              props.data.TeleopScoredSpeaker + 1
-            );
-          }}
-          className="btn btn-outline rounded-none w-full h-full text-lg"
-        >
-          Scored Speaker
-        </button>
-        <button
-          onClick={() => {
-            props.callback("TeleopScoredTrap", props.data.TeleopScoredTrap + 1);
-          }}
-          className="btn btn-outline rounded-none w-full h-full text-lg"
-        >
-          Scored Trap
-        </button>
-
-        <button
-          onClick={() => {
-            props.callback("TeleopMissedAmp", props.data.TeleopMissedAmp + 1);
-          }}
-          className="btn btn-outline rounded-none rounded-bl-xl w-full h-full text-lg"
-        >
-          Missed Amp
-        </button>
-        <button
-          onClick={() => {
-            props.callback(
-              "TeleopMissedSpeaker",
-              props.data.TeleopMissedSpeaker + 1
-            );
-          }}
-          className="btn btn-outline rounded-none w-full h-full text-lg"
-        >
-          Missed Speaker
-        </button>
-        <button
-          onClick={() => {
-            props.callback("TeleopMissedTrap", props.data.TeleopMissedTrap + 1);
-          }}
-          className="btn btn-outline rounded-none w-full h-full text-lg rounded-br-xl"
-        >
-          Missed Trap
-        </button>
+      <div className="w-full h-2/3 grid grid-cols-3 grid-rows-2 mt-4">
+        <IncrementButton dataKey="TeleopScoredAmp" data={props.data} callback={props.callback} text="Scored Amp" rounded="tl" />
+        <IncrementButton dataKey="TeleopScoredSpeaker" data={props.data} callback={props.callback} text="Scored Speaker" />
+        <IncrementButton dataKey="TeleopScoredTrap" data={props.data} callback={props.callback} text="Scored Trap" rounded="tr" />
+        <IncrementButton dataKey="TeleopMissedAmp" data={props.data} callback={props.callback} text="Missed Amp" rounded="bl" />
+        <IncrementButton dataKey="TeleopMissedSpeaker" data={props.data} callback={props.callback} text="Missed Speaker" />
+        <IncrementButton dataKey="TeleopMissedTrap" data={props.data} callback={props.callback} text="Missed Trap" rounded="br" />
       </div>
     </div>
   );
