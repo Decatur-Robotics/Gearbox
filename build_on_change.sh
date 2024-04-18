522a8ca46bf7d92f272ee32f5230e63afefc4d51
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-04-18 16:56:57 -0400

    Added new log

--- build_on_change.sh
+++ build_on_change.sh
@@ -7,4 +7,5 @@ if [[ "$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]]; the
   rm -rf ./cd.log
   echo "Deploying new changes at $(date): $(git log -1 --pretty=%B)"
   bash update.sh &
+  echo "Ran update.sh in background"
 fi
