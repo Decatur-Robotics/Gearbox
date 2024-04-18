6f82e803b6b46b7e7ac3e00efb2c89bd5ca4727f
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-04-18 16:50:27 -0400

    Changed label

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ git stash
 git pull
 git stash apply
 NODE=/home/ubuntu/.nvm/versions/node/v20.12.0/bin/npm
-echo "Running on Node $($NODE --version)"
+echo "Running on NPM $($NODE --version)"
 $NODE ci
 $NODE run build
 pid=$(sudo lsof -t -i:443)
