554916881415893ea10326aed01f4eec3d74ce34
renatodellosso <rddellosso4@gmail.com>
2024-06-25 12:12:17 -0400

    Changes from previous commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,8 +24,7 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
         <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
 
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/public/owenToPng.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
         <link rel="shortcut icon" href="/favicon.ico" />
---
+++ pages/_offline.tsx
@@ -0,0 +1,8 @@
+export default function Offline() {
+  return (
+    <div>
+      <h1>Offline</h1>
+      <p>Sorry, you are offline</p>
+    </div>
+  );
+}
