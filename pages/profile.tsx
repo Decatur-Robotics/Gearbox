8178f7541f891c0c039b91994ca31a3e821f7d58
renatodellosso <rddellosso4@gmail.com>
2024-06-26 14:54:05 -0400

    Fixed update modal

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -59,7 +59,7 @@ export default function Profile(props: { teamList: Team[] }) {
 
   return (
     <Container requireAuthentication={true} hideMenu={false}>
-      <UpdateModal></UpdateModal>
+      <UpdateModal />
       <Flex className="my-8 space-y-4" center={true} mode="col">
         <Card title={user?.name} coloredTop="bg-accent">
           <Flex
