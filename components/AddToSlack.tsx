8285497c549e35907f5840f5f13190c0c1105ff7
renatodellosso <rddellosso4@gmail.com>
2024-07-11 17:09:56 -0400

    Changed Add to Slack button

--- components/AddToSlack.tsx
+++ components/AddToSlack.tsx
@@ -1,8 +1,7 @@
 export default function AddToSlack() {
   return (
     <a href="https://slack.com/oauth/v2/authorize?client_id=10831824934.7404945710466&scope=chat:write,commands&user_scope=chat:write,email,openid">
-      <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" 
-        srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+      Add Gearbox to Slack
     </a>
   );
 }
