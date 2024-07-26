3e9ca4916ad274a8863dd264585eb7641cdcd959
renatodellosso <rddellosso4@gmail.com>
2024-07-26 19:22:57 -0400

    Autofilled redirect URL

--- components/AddToSlack.tsx
+++ components/AddToSlack.tsx
@@ -1,10 +1,14 @@
+import { useRouter } from "next/router";
+
 export default function AddToSlack() {
+  const host = window ? window.location.host : "4026.org";
+
   return (
     <a className="link text-accent" href={`
         https://slack.com/oauth/v2/authorize
           ?client_id=${process.env.NEXT_PUBLIC_SLACK_CLIENT_ID}
           &scope=chat:write,commands&user_scope=email,openid
-          &redirect_uri=https%3A%2F%2Flocalhost%2Fapi%2FaddSlackBot
+          &redirect_uri=https%3A%2F%2F${host}/api/addSlackBot%2Fapi%2FaddSlackBot
     `}>
       Add Gearbox to Slack
     </a>
