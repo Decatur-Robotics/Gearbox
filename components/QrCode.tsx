3e33b348ace02f1294ce9eacca751e4ee72e5b0a
renatodellosso <rddellosso4@gmail.com>
2024-08-15 16:57:11 -0400

    Thinner QR padding

--- components/QrCode.tsx
+++ components/QrCode.tsx
@@ -1,7 +1,7 @@
 import QRCode from "react-qr-code";
 
 export default function QrCode(props: { value: string }) {
-  return <div className="bg-white p-16">
+  return <div className="bg-white p-8">
     <QRCode value={props.value} />
   </div>
 }
