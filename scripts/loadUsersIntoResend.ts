22551e74d5f14e8fcbef4fc5213eccc3fbd8b277
renatodellosso <rddellosso4@gmail.com>
2024-08-30 23:29:43 -0400

    Created Load Users Into Resend script

---
+++ scripts/loadUsersIntoResend.ts
@@ -0,0 +1,25 @@
+import { Collections, getDatabase } from "@/lib/MongoDB";
+import ResendUtils from "@/lib/ResendUtils";
+import { User } from "@/lib/Types";
+
+async function loadUsersIntoResend() {
+  console.log("Loading users into Resend...");
+
+  console.log("Getting database...");
+  const db = await getDatabase();
+  
+  console.log("Finding users...");
+  const users = await db.findObjects<User>(Collections.Users, {});
+
+  console.log(`Saving ${users.length} users to Resend...`);
+  users.forEach(async user => {
+    if (user.email == "totallyrealemail@gmail.com")
+      return;
+    
+    ResendUtils.createContact(user);
+  });
+
+  console.log("Done!");
+}
+
+loadUsersIntoResend();
