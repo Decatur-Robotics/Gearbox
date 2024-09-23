c718e08e969558b8b588026cb1319a4b2a2f8f6c
renatodellosso <rddellosso4@gmail.com>
2024-09-23 18:01:09 -0400

    Testing fail cases. Also more tests

---
+++ tests/ClientUtils.test.ts
@@ -0,0 +1,35 @@
+import { camelCaseToTitleCase, removeDuplicates, toDict } from "@/lib/client/ClientUtils";
+
+test("camelCaseToTitleCase", () => {
+  expect(camelCaseToTitleCase("notLinkedToTba")).toBe("Not Linked To Tba");
+});
+
+test("toDict", () => {
+  const array = [
+    { _id: "1", name: "one" },
+    { _id: "2", name: "two" },
+    { _id: "3", name: "three" }
+  ]
+
+  const dict = {
+    "1": { _id: "1", name: "one" },
+    "2": { _id: "2", name: "two" },
+    "3": { _id: "3", name: "three" }
+  }
+
+  expect(toDict(array)).toEqual(dict);
+});
+
+test("removeDuplicates: 1D array", () => {
+  const arr = [1, 2, 3, 4, 1, 2, 3, 4, 5];
+  expect(removeDuplicates(arr)).toEqual([1, 2, 3, 4, 5]);
+});
+
+test("removeDuplicates: 2D array", () => {
+  const arr = [[1, 2], [3, 4], [1, 2], [3, 4], [5]];
+  expect(removeDuplicates(arr)).toEqual([1, 2, 3, 4, 5]);
+});
+
+test("Fail", () => {
+  expect(true).toBe(false);
+});
