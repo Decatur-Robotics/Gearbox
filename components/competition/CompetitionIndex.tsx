7f05472cd146809110d5c9df80d1cf69472c13c8
renatodellosso <rddellosso4@gmail.com>
2024-10-18 12:27:48 -0400

    Fixed comp page spacing

--- components/competition/CompetitionIndex.tsx
+++ components/competition/CompetitionIndex.tsx
@@ -595,7 +595,7 @@ export default function CompetitionIndex(props: {
 
   return (
     <>
-      <div className="min-h-screen w-full flex flex-col sm:flex-row flex-grow justify-center sm:space-x-6 my-4 sm:my-4">
+      <div className="min-h-screen w-full flex flex-col sm:flex-row flex-grow justify-center sm:space-x-6 my-4">
         <div className="w-full sm:w-2/5 flex flex-col items-center flex-grow justify-center space-y-4 h-full">
           <CompHeaderCard comp={comp} openDownloadModal={openDownloadModal} isOnline={isOnline} />
           <InsightsAndSettingsCard 
@@ -612,7 +612,7 @@ export default function CompetitionIndex(props: {
           />
         </div>
 
-        <div className="w-full sm:w-1/2 flex flex-col flex-grow justify-center h-screen space-y-4">
+        <div className="w-full sm:w-1/2 flex flex-col flex-grow h-screen space-y-4">
           <MatchScheduleCard 
             matches={matches}
             usersById={usersById} reportsById={reportsById} comp={comp} isManager={isManager} openEditMatchModal={openEditMatchModal}
