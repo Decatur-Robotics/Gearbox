85bb43446f0055fa99c3b17062511f6adcc06faf
renatodellosso <rddellosso4@gmail.com>
2024-10-24 16:33:13 -0400

    Better speed test

--- pages/speedtest.tsx
+++ pages/speedtest.tsx
@@ -1,10 +1,13 @@
 import Container from "@/components/Container";
+import Loading from "@/components/Loading";
 import ClientAPI from "@/lib/client/ClientAPI";
+import { Round } from "@/lib/client/StatsMath";
 import { useEffect, useState } from "react";
 
 const api = new ClientAPI("gearboxiscool");
 
 type SpeedTestResponse = { 
+  [key: string]: number,
   requestTime: number,
   authTime: number,
   insertTime: number,
@@ -12,33 +15,77 @@ type SpeedTestResponse = {
   updateTime: number,
   deleteTime: number,
   responseTime: number, 
+  dbTime: number,
+  totalTime: number
 };
+
+const SPEED_TEST_LENGTH = 10;
 
 export default function SpeedTest() {
   const [times, setTimes] = useState<SpeedTestResponse>();
+  const [resultsCompleted, setResultsCompleted] = useState<number | undefined>(undefined);
+
+  async function runSpeedTest() {
+    setResultsCompleted(undefined);
+
+    const newResults: SpeedTestResponse[] = [];
+
+    while (newResults.length < SPEED_TEST_LENGTH) {
+      const newTimes = await api.speedTest();
+      newResults.push({
+        ...newTimes,
+        dbTime: newTimes.insertTime + newTimes.findTime + newTimes.updateTime + newTimes.deleteTime,
+        totalTime: Object.values(newTimes).reduce((acc, time) => acc + time, 0)
+      });
+      setResultsCompleted(newResults.length);
+    }
+    
+    const avgTimes: typeof times = newResults.reduce((acc, times) => {
+      Object.entries(times).forEach(([key, value]) => {
+        acc[key] += value;
+      });
+      return acc;
+    }, {
+      requestTime: 0,
+      authTime: 0,
+      insertTime: 0,
+      findTime: 0,
+      updateTime: 0,
+      deleteTime: 0,
+      responseTime: 0,
+      dbTime: 0,
+      totalTime: 0
+    });
+
+    Object.keys(avgTimes).forEach(key => {
+      avgTimes[key] /= newResults.length;
+    });
+
+    setTimes(avgTimes);
+    setResultsCompleted(undefined);
+  }
 
   useEffect(() => {
-    api.speedTest().then(setTimes);
+    runSpeedTest();
   }, []);
 
   return (
     <Container requireAuthentication={true} title={"Speed Test"}>
+      { resultsCompleted !== undefined && <progress value={resultsCompleted} max={SPEED_TEST_LENGTH} className="progress progress-primary" /> }
       {
         times 
           ? (
             <div>
               {Object.entries(times).map(([key, value]) => (
                 <div key={key}>
-                  {key}: {value}ms
+                  {key}: {Round(value)}ms ({Round(value / times.totalTime * 100)}%)
                 </div>
               ))}
-              <div>
-                Total: {Object.values(times).reduce((a, b) => a + b, 0)}ms
-              </div>
             </div>
           )
           : <div>Loading...</div>
       }
+      <button onClick={runSpeedTest} className="btn btn-primary">Run Speed Test</button>
     </Container>
   );
 }
