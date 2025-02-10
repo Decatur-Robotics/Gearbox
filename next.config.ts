f98a7947c97c3a98510d2fbe81f2e6ea5aecd679
renatodellosso <rddellosso4@gmail.com>
2025-02-10 17:19:00 -0500

    Fix offline page revisioning

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import packageConfig from "./package.json";
 import withSerwistInit from "@serwist/next";
 
-console.log(process.env.NEXT_PUBLIC_BUILD_TIME);
 const withSerwist = withSerwistInit({
 	swSrc: "lib/sw.ts",
 	swDest: "public/sw.js",
 	additionalPrecacheEntries: [
-		{ url: "/offline", revision: process.env.NEXT_PUBLIC_BUILD_TIME },
+		{ url: "/offline", revision: new Date().toDateString() },
 	],
 });
 
