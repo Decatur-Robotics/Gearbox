7a5e5c10b6a2be8ba40da48453df64efbe50a999
Teknosquad5219 <colin.wells.desimone@gmail.com>
2025-02-13 16:50:01 -0500

    Lint fix maybe???

--- components/forms/FieldPositionSelector.tsx
+++ components/forms/FieldPositionSelector.tsx
@@ -30,7 +30,7 @@ export default function FieldPositionSelector({
 
 	useEffect(() => {
 		callback("AutoStart", { x: mx ?? 0, y: my ?? 0, angle: a ?? 0 });
-	}, [mx, my, a, ax, ay, callback.name]);
+	}, [mx, my, a, ax, ay, callback]);
 
 	const setup = (p5: p5Types, canvasParentRef: Element) => {
 		bg = p5.loadImage(
