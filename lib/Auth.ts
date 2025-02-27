2b6565fc391471765e4c12bd0a032d64fe41187e
renatodellosso <rddellosso4@gmail.com>
2025-02-27 18:14:07 -0500

    Less logs in Auth

--- lib/Auth.ts
+++ lib/Auth.ts
@@ -97,7 +97,6 @@ export const AuthenticationOptions: AuthOptions = {
 	],
 	callbacks: {
 		async session({ session, user }) {
-			console.log("Session callback:", session, user);
 			session.user = user;
 
 			return session;
