767ccb19d58a42c8f2a8f3677a4694e3d1b8bcf6
Davis Becker <143132652+BanEvading@users.noreply.github.com>
2025-03-17 17:06:36 -0400

    Adding rounding to the pitstats page

--- pages/[teamSlug]/[seasonSlug]/[competitonSlug]/pitstats.tsx
+++ pages/[teamSlug]/[seasonSlug]/[competitonSlug]/pitstats.tsx
@@ -27,6 +27,7 @@ import { games } from "@/lib/games";
 import { PitStatsLayout, Badge } from "@/lib/Layout";
 import CollectionId from "@/lib/client/CollectionId";
 import { matchesMiddleware } from "next/dist/shared/lib/router/router";
+import { Round } from '../../../../lib/client/StatsMath';
 
 const api = new ClientApi();
 
@@ -131,7 +132,7 @@ function TeamSlide(props: {
 			return (
 				<div key={index}>
 					<p className="text-lg">
-						{stat.label}: <span className="text-accent">{stat.value}</span>{" "}
+						{stat.label}: <span className="text-accent">{Math.round(stat.value)}</span>{" "}
 						<span className="text-primary">
 							(Ranked #{stat.rank}/{stat.maxRanking})
 						</span>
