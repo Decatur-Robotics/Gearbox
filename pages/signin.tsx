802130ed89dfcec6ffe982be91b37f2f2aae3082
renatodellosso <rddellosso4@gmail.com>
2025-03-21 09:08:36 -0400

    Refactor PitScoutingCard to replace image with check icon and clean up sign-in page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,13 +1,5 @@
 import Container from "@/components/Container";
 import SignInMenu from "@/components/SignInMenu";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/router";
-import { useCallback, useEffect, useRef, useState } from "react";
-import {
-	GoogleReCaptchaProvider,
-	useGoogleReCaptcha,
-} from "react-google-recaptcha-v3";
-import { FaGoogle, FaSlack } from "react-icons/fa";
 
 export default function SignIn() {
 	return (
